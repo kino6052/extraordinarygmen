81459788f113aae7a843e14c2fa02eb8676ed2f9
kino6052 <kino6052@colorado.edu>
2016-08-21 21:32:57 -0600

    Initial Commit

---
+++ Assets/Scripts/Player/PlatformerCharacter2D.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+namespace Game.Player
+{
+
+    public class PlatformerCharacter2D : MonoBehaviour
+    {
+        private bool facingRight = true; // For determining which way the player is currently facing.
+
+        [SerializeField] private float maxSpeed = 10f; // The fastest the player can travel in the x axis.
+        [SerializeField] private float jumpForce = 10f; // Amount of force added when the player jumps.	
+
+        [Range(0, 1)] [SerializeField] private float crouchSpeed = .36f;
+                                                     // Amount of maxSpeed applied to crouching movement. 1 = 100%
+
+        [SerializeField] private bool airControl = false; // Whether or not a player can steer while jumping;
+        [SerializeField] private LayerMask whatIsGround; // A mask determining what is ground to the character
+
+        private Transform groundCheck; // A position marking where to check if the player is grounded.
+        private float groundedRadius = 1f; // Radius of the overlap circle to determine if grounded
+        private bool grounded = false; // Whether or not the player is grounded.
+        private Transform ceilingCheck; // A position marking where to check for ceilings
+        private float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
+        private Animator anim; // Reference to the player's animator component.
+
+
+        private void Awake()
+        {
+            // Setting up references.
+            groundCheck = transform.Find("GroundCheck");
+            ceilingCheck = transform.Find("CeilingCheck");
+            anim = GetComponent<Animator>();
+        }
+
+
+        private void FixedUpdate()
+        {
+            // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+            grounded = Physics.OverlapSphere(groundCheck.position, groundedRadius, whatIsGround).Length > 0 ? true : false;
+            anim.SetBool("Ground", grounded);
+
+            // Set the vertical animation
+            anim.SetFloat("vSpeed", GetComponent<Rigidbody>().velocity.y);
+        }
+
+
+
+        public void Move(float moveH, float moveV, bool crouch, bool jump)
+        {
+
+
+            // If crouching, check to see if the character can stand up
+            if (!crouch && anim.GetBool("Crouch"))
+            {
+                // If the character has a ceiling preventing them from standing up, keep them crouching
+                if (Physics.OverlapSphere(ceilingCheck.position, ceilingRadius, whatIsGround).Length > 0 ? true : false)
+                    crouch = true;
+            }
+
+            // Set whether or not the character is crouching in the animator
+            anim.SetBool("Crouch", crouch);
+
+            //only control the player if grounded or airControl is turned on
+            if (grounded)
+            {
+                // Reduce the speed if crouching by the crouchSpeed multiplier
+                moveH = (crouch ? moveH * crouchSpeed : moveH);
+                moveV = (crouch ? moveV * crouchSpeed : moveV);
+
+                // The Speed animator parameter is set to the absolute value of the horizontal input.
+                anim.SetFloat("Speed", Mathf.Abs(moveH) + Mathf.Abs(moveV));
+
+                // Move the character
+                GetComponent<Rigidbody>().velocity = new Vector3(moveH * maxSpeed, GetComponent<Rigidbody>().velocity.y);
+                GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, GetComponent<Rigidbody>().velocity.y, moveV * maxSpeed);
+
+                // If the input is moving the player right and the player is facing left...
+                if (moveH > 0 && !facingRight)
+                    // ... flip the player.
+                    Flip();
+                // Otherwise if the input is moving the player left and the player is facing right...
+                else if (moveH < 0 && facingRight)
+                    // ... flip the player.
+                    Flip();
+            }
+        }
+
+        private void Flip()
+        {
+            // Switch the way the player is labelled as facing.
+            facingRight = !facingRight;
+
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
